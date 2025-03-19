5a384edb640dedee02e8ad46ae26b528f86501cd
CodeMaven22 <tawahcngwira96@gmail.com>
2025-03-19 23:22:50 +0200

    initial commit

---
+++ src/app/(admin)/admin/layout.tsx
@@ -0,0 +1,27 @@
+import { Raleway } from "next/font/google";
+import {ReactNode} from "react";
+import "../../globals.css";
+import {AdminSidebar} from "@/components/admin/admin-sidebar";
+import {AdminHeader} from "@/components/admin/admin-header";
+
+const raleway = Raleway({
+    subsets: ["latin"],
+    weight: "400"
+})
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
+    return(
+        <html lang="en">
+        <body className={`${raleway.className}`}>
+        <div className=" min-h-screen bg-gray-100 flex">
+            <AdminSidebar />
+            <div className="flex-1 flex flex-col overflow-hidden">
+                <AdminHeader />
+                <main className="flex-1 overflow-y-auto p-4 md:p-6">{children}</main>
+            </div>
+        </div>
+        </body>
+
+        </html>
+    )
+}
