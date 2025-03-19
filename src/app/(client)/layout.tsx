0066e4125f7034f985a56cfc192a42ed605b1a93
CodeMaven22 <tawahcngwira96@gmail.com>
2025-03-19 20:17:29 +0200

    initial commit

---
+++ src/app/(client)/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import {Geist, Geist_Mono, Raleway} from "next/font/google";
+import "../globals.css";
+
+const raleway = Raleway({
+    subsets: ["latin"],
+    weight: "400"
+})
+
+export const metadata: Metadata = {
+  title: "Malawi Shades | Discover the Warm Heart of Africa",
+  description: "Your premier platform for finding accommodations and tourism experiences in Malawi",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.className} flex min-h-screen flex-col `}>
+        {children}
+      </body>
+    </html>
+  );
+}
